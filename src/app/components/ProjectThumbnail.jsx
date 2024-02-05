fc53f50827295dfe4e3701810fdb50710e716873
Ryan Foster-Hill <rfoster-hill@hotmail.com>
2024-02-05 16:51:27 +0000

    changed projects page UI

--- src/app/components/ProjectThumbnail.jsx
+++ src/app/components/ProjectThumbnail.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 
 const ProjectThumbnail = (props) => {
     return (
-        <div className='flex flex-col items-center justify-center max-w-[800px] md:min-h-[500px] mb-[30px] md:mb-[50px] py-[20px] md:px-[30px] md:rounded-3xl bg-blue-200 lg:min-w-[800px]'>
-            <div>
-                <a href={props.projLink} className='w-full flex items-center justify-center hover:opacity-60'>
-                    <div className='absolute font-bebas text-3xl z-10 lg:text-[60px] bg-blue-600 py-[10px] px-[20px] lg:p-[30px] rounded-2xl'>
-                        <p className='text-white'>{props.projName}</p>
-                    </div>
+        <a href={props.projLink} className='w-full flex flex-col items-center justify-center hover:opacity-60'>
 
-                    <img className='max-h-[400px] rounded-3xl' src={props.projImg} alt='portfolio-thumbnail'/>
-                </a>
-            </div>
+        <p className='flex leading-[60px] text-center items-center justify-center font-bebas text-3xl lg:text-[60px] w-full min-h-[100px] lg:rounded-t-3xl text-white bg-blue-400'>{props.projName}</p>
+
+        <div className='flex flex-col items-center justify-center max-w-[800px] md:min-h-[500px] mb-[30px] md:mb-[50px] py-[20px] md:px-[30px] md:rounded-b-3xl bg-blue-200 lg:min-w-[800px] lg:min-h-[500px]'>
+
+            <img className='max-h-[400px] rounded-3xl' src={props.projImg} alt='portfolio-thumbnail' />
+
             <p className='mx-[10px] font-ubuntu font-bold mt-[10px]'>{props.projDesc}</p>
+
         </div>
+    </a>
 
     );
 };
